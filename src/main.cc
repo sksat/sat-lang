639523aa77e1d9a1a87e2d1436f8d840b91c449a
sksat <sksat.tec@gmail.com>
2018-06-23 22:19:04 +0900

    [UPDATE]

--- src/main.cc
+++ src/main.cc
@@ -4,21 +4,25 @@
 
 int main(int argc, char **argv){
 	source src("test.sat");
+	src.set_tokenizer(tokenizer::sat);
 
 	std::cout
-		<< "***** source code *****"
-		<< std::endl
+		<< "***** source code *****" << std::endl
 		<< src.get_src()
 		<< "***********************"
 		<< std::endl << std::endl;
 
-	src.set_tokenizer(tokenizer::sat);
-
+	std::cout
+		<< "******** token ********" << std::endl;
 	while(true){
 		auto token = src.get_token();
 		if(token.empty()) break;
-		std::cout << "[" << token << "]" << std::endl;
+		std::cout << "[" << token << "] ";
+		if(token == ";" || token == "{" || token == "}")
+			std::cout<<std::endl;
 	}
+	std::cout << std::endl
+		<< "***********************" << std::endl << std::endl;
 
 	return 0;
 }
